513e31fd16433718d76e5b89cc58afceecd9a4c5
lizaveta56k <lizaveta.kaliaha@gmail.com>
2022-05-19 00:33:28 +0200

    change styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import 'components/App/App.css';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import pink from '@material-ui/core/colors/pink';
+import blue from '@material-ui/core/colors/blue';
+
 import PageProducts from "components/pages/PageProducts/PageProducts";
 import MainLayout from "components/MainLayout/MainLayout";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,35 +17,45 @@ import PageOrders from "components/pages/PageOrders/PageOrders";
 import PageOrder from "components/pages/PageOrder/PageOrder";
 import PageProductImport from "components/pages/admin/PageProductImport/PageProductImport";
 
-function App() {
+import 'components/App/App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    secondary: blue,
+  },
+});
+
+function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <MainLayout>
-            <Route exact path="/">
-              <PageProducts/>
-            </Route>
-            <Route exact path={["/admin/product-form/:id", '/admin/product-form']}>
-              <PageProductForm/>
-            </Route>
-            <Route exact path="/cart">
-              <PageCart />
-            </Route>
-            <Route exact path="/admin/orders">
-              <PageOrders />
-            </Route>
-            <Route exact path="/admin/order/:id">
-              <PageOrder />
-            </Route>
-            <Route exact path="/admin/products">
-              <PageProductImport />
-            </Route>
-          </MainLayout>
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <MainLayout>
+              <Route exact path="/">
+                <PageProducts />
+              </Route>
+              <Route exact path={["/admin/product-form/:id", '/admin/product-form']}>
+                <PageProductForm />
+              </Route>
+              <Route exact path="/cart">
+                <PageCart />
+              </Route>
+              <Route exact path="/admin/orders">
+                <PageOrders />
+              </Route>
+              <Route exact path="/admin/order/:id">
+                <PageOrder />
+              </Route>
+              <Route exact path="/admin/products">
+                <PageProductImport />
+              </Route>
+            </MainLayout>
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
